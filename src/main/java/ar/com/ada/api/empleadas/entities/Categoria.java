d2d630e2146494b068e9ddfc0db688949d042ef8
lulinieva <evelynieva@hotmail.com>
2021-06-23 17:44:05 -0300

    Se agregaron las anotaciones para persistir en la Base de Datos.

--- src/main/java/ar/com/ada/api/empleadas/entities/Categoria.java
+++ src/main/java/ar/com/ada/api/empleadas/entities/Categoria.java
@@ -1,14 +1,42 @@
 package ar.com.ada.api.empleadas.entities;
 
 import java.math.BigDecimal;
+import java.util.*;
 
+import javax.persistence.*;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+
+
+@Entity
+@Table(name = "categoria")
 public class Categoria {
-
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "categoria_id")
     private Integer categoriaId;
-
+    
+    
     private String nombre;
 
+    @Column(name = "sueldo_base")
     private BigDecimal sueldoBase;
+
+    @OneToMany(mappedBy = "categoria", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JsonIgnore
+    private List<Empleada> empleadas = new ArrayList<>();
+
+
+
+    public List<Empleada> getEmpleadas() {
+        return empleadas;
+    }
+
+    public void setEmpleadas(List<Empleada> empleadas) {
+        this.empleadas = empleadas;
+    }
 
     public Integer getCategoriaId() {
         return categoriaId;
@@ -34,7 +62,10 @@ public class Categoria {
         this.sueldoBase = sueldoBase;
     }
 
+    public void agregarEmpleada (Empleada empleada){
+        this.empleadas.add(empleada);
+    }
 
+}
 
     
-}
